64abb578189f99ac402d7f9c13b8ea15a61ed3e6
Harkirat <hs3188248@gmail.com>
2023-01-28 13:50:05 +0530

    created backend api corntrollers and routes with mvc

--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -3,15 +3,31 @@ var router = express.Router();
 
 
 const {getAllOrders} = require("../controllers/order_controller")
+const {getOrderById} = require("../controllers/order_controller")
+const {addNewOrder} = require("../controllers/order_controller")
+const {updateOrderById} = require("../controllers/order_controller")
+const {removeOrderById} = require("../controllers/order_controller")
 
-/* GET home page. */
+// Order Routes
+
 router.get('/', function(req, res, next) {
   res.send('Start Creating your app');
 });
 
-
 //status of order
-// router.get('/order',getAllOrders)
+router.get('/order',getAllOrders)
+
+//get Order by id
+router.get("/order/:id",getOrderById)
+
+//add new orders
+router.post("/orders",addNewOrder)
+
+//Update Order by id
+router.put("/orders/:id",updateOrderById)
+
+//Remove Order by Id
+router.delete("/orders/:id",removeOrderById)
 
 
 module.exports = router;
